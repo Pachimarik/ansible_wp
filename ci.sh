f4c731b7cdf14714afd7da4813fe5ce664d4a172
root <root@localhost.localdomain>
2022-07-12 04:27:11 -0400

    first commit

---
+++ ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export $(cat  variables | xargs)
+
+# base requirements
+yum install python39 sshpass -y
+python3.9 -m pip install --upgrade pip --user
+python3.9 -m pip install ansible --user
+
+# ansible work
+cd ansible/
+ansible-config view
+ansible -m ping all
