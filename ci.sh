3a730077ab8cc0faef366f8335ff4f2609018097
Pachimarik <sahok_v@mail.ru>
2022-07-12 11:20:30 -0400

    common settings for centos 8

--- ci.sh
+++ ci.sh
@@ -2,6 +2,9 @@
 export $(cat  variables | xargs)
 
 # base requirements
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
 yum install python39 sshpass -y
 python3.9 -m pip install --upgrade pip --user
 python3.9 -m pip install ansible --user
@@ -10,5 +13,7 @@ python3.9 -m pip install ansible --user
 cd ansible/
 ansible-config view
 ansible -m ping all
+
+ansible-playbook centos8_common.yml
 
 cd ..
