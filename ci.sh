657012d776648f488a36a59a344fa35f6a38c4ac
Pachimarik <sahok_v@mail.ru>
2022-07-13 02:34:38 -0400

    nginx container ready

--- ci.sh
+++ ci.sh
@@ -2,18 +2,20 @@
 export $(cat  variables | xargs)
 
 # base requirements
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+# sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+# sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-yum install python39 sshpass -y
-python3.9 -m pip install --upgrade pip --user
-python3.9 -m pip install ansible --user
+# yum install python39 sshpass -y
+# python3.9 -m pip install --upgrade pip --user
+# python3.9 -m pip install ansible --user
 
 # ansible work
-cd ansible/
-ansible-config view
-ansible -m ping all
+# cd ansible/
+# ansible-config view
+# ansible -m ping all
 
-ansible-playbook centos8_common.yml
+# #ansible-playbook centos8_common.yml
+# ansible-playbook wp_nginx.yml
 
-cd ..
+cd ./files/docker/nginx
+docker-compose up -d
